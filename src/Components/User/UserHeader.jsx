d7ee1c168805f62f35a569520a868c66e7ec7f18
Lucas Henrique Torresin da Costa <82097583+Lunatic02@users.noreply.github.com>
2023-03-20 20:23:24 -0300

    user Header

--- src/Components/User/UserHeader.jsx
+++ src/Components/User/UserHeader.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import UserHeaderNav from './UserHeaderNav';
+import styles from './UserHeader.module.css';
+import { useLocation } from 'react-router';
 
 const UserHeader = () => {
+  const [title, setTitle] = React.useState('');
+  const location = useLocation();
+
+  React.useEffect(() => {
+    setTitle(location.pathname);
+    if (location.pathname === '/conta') {
+      setTitle('Conta');
+    } else if (location.pathname === '/conta/estatisticas') {
+      setTitle('Estatísticas');
+    } else {
+      setTitle('Poste Sua Foto');
+    }
+  }, [location]);
+
   return (
-    <header>
-      <h1 className="title">Título</h1>
+    <header className={styles.header}>
+      <h1 className="title">{title}</h1>
       <UserHeaderNav />
     </header>
   );
--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
 import { ReactComponent as MinhasFotos } from '../../Assets/Feed.svg';
 import { ReactComponent as Estatisticas } from '../../Assets/estatisticas.svg';
 import { ReactComponent as Adicionar } from '../../Assets/adicionar.svg';
+import { ReactComponent as Sair } from '../../Assets/sair.svg';
+import styles from './UserHeaderNav.module.css';
 
 const UserHeaderNav = () => {
+  const [mobile, setMobile] = useState(null);
   const { userLogout } = React.useContext(UserContext);
 
   return (
-    <nav>
-      <NavLink to="/conta">
+    <nav className={styles.nav}>
+      <NavLink to="/conta" end>
         <MinhasFotos />
+        {mobile && 'Minhas Fotos'}
       </NavLink>
       <NavLink to="/conta/estatisticas">
         <Estatisticas />
+        {mobile && 'Estatísticas'}
       </NavLink>
       <NavLink to="/conta/postar">
         <Adicionar />
+        {mobile && 'Adicionar'}
       </NavLink>
-      <button onClick={userLogout}>Sair</button>
+      <button onClick={userLogout}>
+        <Sair /> {mobile && 'Sair'}
+      </button>
     </nav>
   );
 };
