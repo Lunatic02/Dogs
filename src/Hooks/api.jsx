d9b86c41772bd4a960b6e12cabb507c6f00a0527
Lunatic02 <lucas_hcosta@live.com>
2023-03-21 19:16:13 -0300

    Feed Incompleto

--- src/Hooks/api.jsx
+++ src/Hooks/api.jsx
@@ -1,5 +1,6 @@
 export const API_URL = 'https://dogsapi.origamid.dev/json';
 
+// Solicita o token do usuario após autenticação
 export function TOKEN_POST(body) {
   return {
     url: API_URL + '/jwt-auth/v1/token',
@@ -13,6 +14,7 @@ export function TOKEN_POST(body) {
   };
 }
 
+// Verifica se o token é valido
 export function TOKEN_VALIDATE_POST(token) {
   return {
     url: API_URL + '/jwt-auth/v1/token/validate',
@@ -25,6 +27,7 @@ export function TOKEN_VALIDATE_POST(token) {
   };
 }
 
+// Solicita os dados do usuário após informar o token
 export function USER_GET(token) {
   return {
     url: API_URL + '/api/user',
@@ -36,6 +39,8 @@ export function USER_GET(token) {
     },
   };
 }
+
+// Cadastrar um usuario
 export function USER_POST(body) {
   return {
     url: API_URL + '/api/user',
@@ -49,6 +54,7 @@ export function USER_POST(body) {
   };
 }
 
+// Envia as fotos do formulario para a API
 export function PHOTO_POST(formData, token) {
   return {
     url: API_URL + '/api/photo',
@@ -58,6 +64,94 @@ export function PHOTO_POST(formData, token) {
         Authorization: 'Bearer ' + token,
       },
       body: formData,
+    },
+  };
+}
+
+// Solicita as fotos da API de acordo com os parametros passados
+export function PHOTOS_GET({ page, total, user }) {
+  return {
+    url: `${API_URL}/api/photo/?_page=${page}&_total=${total}&_user=${user}`,
+    options: {
+      method: 'GET',
+      cache: 'no-store',
+    },
+  };
+}
+
+// Solicita a foto da API de acordo com o ID
+export function PHOTO_GET(id) {
+  return {
+    url: `${API_URL}/api/photo/${id}`,
+    options: {
+      method: 'GET',
+      cache: 'no-store',
+    },
+  };
+}
+
+// Solicita a foto da API de acordo com o ID
+export function COMMENT_POST(id, body, token) {
+  return {
+    url: `${API_URL}/api/comment/${id}`,
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
+
+// Deleta uma foto de acordo com o ID
+export function PHOTO_DELETE(id, token) {
+  return {
+    url: `${API_URL}/api/photo/${id}`,
+    options: {
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    },
+  };
+}
+
+export function PASSWORD_LOST(body) {
+  return {
+    url: API_URL + '/api/password/lost',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
+
+export function PASSWORD_RESET(body) {
+  return {
+    url: API_URL + '/api/password/reset',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
+
+export function STATS_GET(token) {
+  return {
+    url: API_URL + '/api/stats',
+    options: {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
     },
   };
 }
