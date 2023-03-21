aa29e2f0aefaf08ede189af86fb22309ed99116c
Lucas Henrique Torresin da Costa <82097583+Lunatic02@users.noreply.github.com>
2023-03-20 21:43:53 -0300

    userPhotoPost incompleto

--- src/Components/User/UserPhotoPost.jsx
+++ src/Components/User/UserPhotoPost.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import { PHOTO_POST } from '../../Hooks/api';
+import styles from './UserPhotoPost.module.css';
 
 const UserPhotoPost = () => {
-  return <div>Postar foto</div>;
+  const nome = useForm();
+  const peso = useForm('number');
+  const idade = useForm('number');
+  const [img, setImg] = React.useState({});
+  const { data, error, loading, request } = useFetch();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('img', img.raw);
+    formData.append('nome', nome.value);
+    formData.append('peso', peso.value);
+    formData.append('idade', idade.value);
+
+    const token = window.localStorage.getItem('token');
+    const { url, options } = PHOTO_POST(formData, token);
+    request(url, options);
+  }
+  function handleImgChange({ target }) {
+    setImg({
+      raw: target.files[0],
+    });
+  }
+
+  return (
+    <section className={`${styles.photoPost} animeLeft`}>
+      <form onSubmit={handleSubmit}>
+        <Input label="Nome" type="Text" name="Nome" />
+        <Input label="Peso" type="Text" name="Peso" />
+        <Input label="Idade" type="Text" name="Idade" />
+        <input type="file" name="img" id="img" onChange={handleImgChange} />
+        <Button>Enviar</Button>
+      </form>
+    </section>
+  );
 };
 
 export default UserPhotoPost;
