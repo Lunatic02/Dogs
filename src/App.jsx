34da70730bcdf61744e6b57125ffecc5934335ca
Lunatic02 <lucas_hcosta@live.com>
2023-03-24 20:55:17 -0300

    Perfil Usuários

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { UserStorage } from './UserContext';
 import User from './Components/User/User';
 import ProtectedRoute from './Helper/ProtectedRoute';
 import Photo from './Components/photo/Photo';
+import UserProfile from './Components/User/UserProfile';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
             <Route
               path="conta/*"
               element={
